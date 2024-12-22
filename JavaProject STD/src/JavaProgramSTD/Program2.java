28fcb9a582dce92e12ec8d0cb1e2620860ed7398
Shan <Shan@ShansPC>
2024-12-22 11:54:59 -0500

    testing commit

---
+++ JavaProject STD/src/JavaProgramSTD/Program2.java
@@ -0,0 +1,15 @@
+package JavaProgramSTD;
+
+public class Program2 {
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int a = 100;
+			int b = 23;
+			int c = a-b;
+			System.out.println ("Subtraction " + c);
+		}
+
+	}
+
